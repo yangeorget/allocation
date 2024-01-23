3e1eadc3b06c8db828db2c6024b77e7c2191979c
User <user@ngp3w6n2tx-1.home>
2024-01-23 23:13:49 +0100

    add display

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import random
-
+from matplotlib import pyplot
 import numpy as np
 
 
 def main():
-    exploration_factor = 5
-    user_nb = 1000
-    offer_nb = 100
-    max_offer_nb = 20
+    exploration_factor = 0
+    user_nb = 100
+    offer_nb = 20
+    max_offer_nb = 10
     offer_max_user_nb = np.random.randint(1, user_nb, size=offer_nb)
+    # print(f"offer_max_user_nb={offer_max_user_nb}")
     ok = np.random.rand(user_nb, offer_nb) > (1.0 / offer_nb)
     scores = np.random.random((user_nb, offer_nb))
     Allocator(max_offer_nb, offer_max_user_nb, ok, scores, exploration_factor).optimize()
@@ -24,7 +25,8 @@ class Allocator:
 
     def optimize(self):
         best_score_sum = 0
-        for iteration in range(10000):
+        for iteration in range(100):
+            # print(f"iteration {iteration}")
             permutation, allocations, score_sum = self.solve()
             if score_sum > best_score_sum:
                 best_score_sum = score_sum
@@ -33,9 +35,13 @@ class Allocator:
                 print(f"iteration {iteration}: {best_score_sum}")
         return best_permutation, best_allocations, best_score_sum
 
+    def display(self, allocations):
+        pyplot.matshow(allocations)
+        pyplot.show()
+
     def solve(self):
         user_nb = self.ok.shape[0]
-        permutation = np.random.permutation(user_nb)
+        permutation = np.random.permutation(user_nb) #np.arange(user_nb)
         return self.solve_permutation(permutation)
 
     def solve_permutation(self, permutation):
@@ -43,16 +49,30 @@ class Allocator:
         shuffled_scores = self.scores[permutation]
         shuffled_ok = self.ok[permutation]
         allocations, sum_score = self.solve_constraints(shuffled_ok, shuffled_scores)
+        # inverse_permutation = self.inverse_permutation(permutation)
+        # print(f"inverse_permutation={inverse_permutation}")
+        # self.display(allocations[inverse_permutation])
         return permutation, allocations, sum_score
+
+    def inverse_permutation(self, a):
+        b = np.arange(a.shape[0])
+        b[a] = b.copy()
+        return b
 
     def solve_constraints(self, shuffled_ok, shuffled_scores):
         # print(f"solve_constraints({shuffled_ok}, {shuffled_scores})")
         allocations = shuffled_ok
+        # self.display(allocations)
         scores = shuffled_scores
         update_scores(scores, allocations)
-        update_allocations(allocations, compute_user_allocations(scores, self.max_offer_nb, self.exploration_factor))
+        user_allocations = compute_user_allocations(scores, self.max_offer_nb, self.exploration_factor)
+        # self.display(user_allocations)
+        update_allocations(allocations, user_allocations)
         update_scores(scores, allocations)
-        update_allocations(allocations, compute_offer_allocations(scores, self.offer_max_user_nb))
+        offer_allocations = compute_offer_allocations(scores, self.offer_max_user_nb)
+        # self.display(offer_allocations)
+        update_allocations(allocations, offer_allocations)
+        # self.display(allocations)
         update_scores(scores, allocations)
         assert verify_allocation_ok(allocations, shuffled_ok)
         assert verify_allocation_user_max_offer_nb(allocations, self.max_offer_nb)
