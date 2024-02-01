e86cd5282e6bf8ec3dd976fbde58c8d58dd09344
Yan Georget <yan.georget@relevanc.com>
2024-02-01 18:18:05 +0100

    oopify

--- verifier.py
+++ verifier.py
@@ -11,22 +11,19 @@ class Verifier:
         self.init_allocations = init_allocations
         self.init_scores = init_scores
 
-    def verify_constraint_init_allocations(self, allocations):
-        constraints = np.logical_or(np.logical_not(allocations), self.init_allocations)
-        return np.all(constraints)
+    def verify_constraints_init_allocations(self, allocations):
+        return np.all(np.logical_or(np.logical_not(allocations), self.init_allocations))
 
-    def verify_constraint_user_max_offer_nb(self, allocations):
-        offer_nb_by_user = np.sum(allocations, axis=1)
-        return np.all(offer_nb_by_user <= self.max_offer_nb)
+    def verify_constraints_user_max_offer_nb(self, allocations):
+        return np.all(np.sum(allocations, axis=1) <= self.max_offer_nb)
 
-    def verify_constraint_offer_max_user_nb(self, allocations):
-        user_nb_by_offer = np.sum(np.multiply(allocations, self.init_scores), axis=0)
-        return np.all(user_nb_by_offer <= self.offer_max_user_nb)
+    def verify_constraints_offer_max_user_nb(self, allocations):
+        return np.all(np.sum(np.multiply(allocations, self.init_scores), axis=0) <= self.offer_max_user_nb)
 
     def assert_allocation_constraints(self, allocations):
-        assert self.verify_constraint_offer_max_user_nb(allocations)
-        assert self.verify_constraint_user_max_offer_nb(allocations)
-        assert self.verify_constraint_init_allocations(allocations)
+        assert self.verify_constraints_offer_max_user_nb(allocations)
+        assert self.verify_constraints_user_max_offer_nb(allocations)
+        assert self.verify_constraints_init_allocations(allocations)
 
     def assert_file(self, name):
         allocations = np.loadtxt(name, delimiter=",", dtype=bool)
--- verifier_tests.py
+++ verifier_tests.py
@@ -3,26 +3,26 @@ import numpy as np
 from allocator import Verifier
 
 
-def test_verify_constraint_init_allocations():
+def test_verify_constraints_init_allocations():
     ok = np.array([[True, False, False], [False, True, True]])
     verifier = Verifier(None, None, ok, None)
     good_allocations = np.array([[True, False, False], [False, True, False]])
-    verifier.verify_constraint_init_allocations(good_allocations)
+    verifier.verify_constraints_init_allocations(good_allocations)
     bad_allocations = np.array([[True, True, False], [False, True, False]])
-    assert not verifier.verify_constraint_init_allocations(bad_allocations)
+    assert not verifier.verify_constraints_init_allocations(bad_allocations)
 
 
-def test_verify_allocations_user_max_offer_nb():
+def test_verify_constraints_user_max_offer_nb():
     good_allocations = np.array([[True, False, False], [False, True, False]])
     verifier = Verifier(1, None, None, None)
-    assert verifier.verify_constraint_user_max_offer_nb(good_allocations)
+    assert verifier.verify_constraints_user_max_offer_nb(good_allocations)
     bad_allocations = np.array([[True, True, False], [False, True, False]])
-    assert not verifier.verify_constraint_user_max_offer_nb(bad_allocations)
+    assert not verifier.verify_constraints_user_max_offer_nb(bad_allocations)
 
 
-def test_verify_allocation_offer_max_user_nb():
+def test_verify_constraints_offer_max_user_nb():
     verifier = Verifier(None, np.array([[0.3, 0.3, 0.3]]), None, np.array([[0.3, 0.2, 0.1], [0.1, 0.2, 0.3]]))
     good_allocations = np.array([[True, False, False], [False, True, False]])
-    assert verifier.verify_constraint_offer_max_user_nb(good_allocations)
+    assert verifier.verify_constraints_offer_max_user_nb(good_allocations)
     bad_allocations = np.array([[True, True, False], [False, True, False]])
-    assert not verifier.verify_constraint_offer_max_user_nb(bad_allocations)
+    assert not verifier.verify_constraints_offer_max_user_nb(bad_allocations)
